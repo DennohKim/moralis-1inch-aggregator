e702c92352b69006c0dc7021ed71653ed2c794b7
DennohKim <kimathidennis38@gmail.com>
2023-02-09 20:46:06 +0300

    feat: add links

--- dex/src/App.js
+++ dex/src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
-
+import Swap from "./components/Swap";
+import Tokens from "./components/Tokens";
 import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className="mainWindow">
+        <Routes>
+          <Route path="/" element={<Swap />}/>
+          <Route path="/tokens" element={<Tokens />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Logo from "../moralis-logo.svg";
 import Eth from "../eth.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -8,17 +9,19 @@ function Header() {
       <header>
         <div className="leftH">
           <img src={Logo} alt="logo" className="logo" />
-          <div className="headerItem">Swap</div>
-          <div className="headerItem">Tokens</div>
+          <Link to="/" className="link">
+            <div className="headerItem">Swap</div>
+          </Link>
+          <Link className="link" to="/tokens">
+            <div className="headerItem">Tokens</div>
+          </Link>
         </div>
         <div className="rightH">
           <div className="headerItem">
             <img src={Eth} alt="eth" className="eth" />
             Ethereum
           </div>
-          <div className="connectButton">
-            Connect
-          </div>
+          <div className="connectButton">Connect</div>
         </div>
       </header>
     </div>
